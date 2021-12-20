9e0df13319e380750f81b6b580c19b8cd1b1bbdb
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2021-12-20 00:43:41 +0000

    Completed first tutorial

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,123 @@
-//IMPORTANT: Includes must be in this order:
+#pragma region Includes
+// IMPORTANT: Includes must be in this order:
 #include <d3d11.h>
 #include <dxgi.h>
+#include <D3DX11.h>
 #include <windows.h>
+#include <dxerr.h>
+#pragma endregion
 
-int WINAPI WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hPrev, _In_ LPSTR lpCmd, _In_ int nCmdShow)
+#pragma region GlobalVars
+HINSTANCE g_hInst = NULL;
+HWND g_hWnd = NULL;
+WCHAR g_WindowName[100] = L"CGP600 - AE2";
+#pragma endregion
+
+#pragma region ForwardDeclarations
+HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+#pragma endregion
+
+#pragma region EntryPoint
+// Entry point
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-    D3D11CreateDeviceAndSwapChain(  NULL,
-                                    D3D_DRIVER_TYPE_REFERENCE,
-                                    NULL,
-                                    NULL,
-                                    NULL,
-                                    NULL,
-                                    D3D11_SDK_VERSION,
-                                    NULL,
-                                    NULL,
-                                    NULL,
-                                    NULL,
-                                    NULL  );
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    if (FAILED(InitialiseWindow(hInstance, nCmdShow)))
+    {
+        DXTRACE_MSG(L"Failed to create window");
+        return 0;
+    }
+
+    // Main message loop
+    MSG msg = { 0 };
+
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        else
+        {
+            // Do something
+        }
+    }
+
+    return (int)msg.wParam;
+}
+#pragma endregion
+
+#pragma region CreateWindow
+// Register class and create window
+HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow)
+{
+    // Give app name
+    WCHAR Name[100] = L"CGP600 - AE2 Game\0";
+
+    // Register class
+    WNDCLASSEX wcex = { 0 };
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    // Needed for non-3D apps
+    //wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    
+    wcex.lpszClassName = Name;
+
+    if (!RegisterClassEx(&wcex)) { return E_FAIL; }
+
+    // Create window
+    g_hInst = hInstance;
+    RECT rc = { 0, 0, 640, 480 };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    g_hWnd = CreateWindow(Name, g_WindowName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                          rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+
+    if (!g_hWnd) { return E_FAIL; }
+    
+    ShowWindow(g_hWnd, nCmdShow);
+
+    return S_OK;
+}
+#pragma endregion
+
+#pragma region RecieveMessage
+// Called every time the app recieves a message
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_KEYDOWN:
+        if (wParam == VK_ESCAPE)
+        {
+            DestroyWindow(g_hWnd);
+            return 0;
+        }
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
 
     return 0;
 }
+#pragma endregion
