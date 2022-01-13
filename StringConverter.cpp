2264cb55f38fcf0a87ef20223e5cbb5f63139365
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2022-01-13 07:13:21 +0000

    Removed unused files

--- StringConverter.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "StringConverter.h"
-
-std::wstring StringConverter::StringToWide(std::string _string)
-{
-    std::wstring wideString(_string.begin(), _string.end());
-
-    return wideString;
-}
